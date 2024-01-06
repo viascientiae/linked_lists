777e97bc8ed83743844a2a02f4187f9b5223b571
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2024-01-06 18:06:13 +0530

    Create scaffold of classes LinkedList and Node

---
+++ linked_lists.rb
@@ -0,0 +1,74 @@
+class LinkedList
+
+  def initialize
+
+  end
+
+  # adds a new node containing 'value' to the end of the list
+  def append(value)
+
+  end
+
+  # adds a new node containing 'value' to the start of the list
+  def prepend(value)
+
+  end
+
+  # returns the total number of nodes in the list
+  def size
+
+  end
+
+  # returns the first node in the list
+  def head
+
+  end
+
+  # returns the last node in the list
+  def tail
+
+  end
+
+  # returns the node at the given index
+  def at(index)
+
+  end
+
+  # removes the last element from the list
+  def pop
+
+  end
+
+  # returns true if the passed in value is in the list and otherwise returns false
+  def contains?(value)
+
+  end
+
+  #returns the index of the node containing value, or nil if not found
+  def find(value)
+
+  end
+
+  # represent your LinkedList objects as strings, so you can print them out and preview them in the console
+  # the format should be: ( value ) -> ( value ) ->  ( value ) -> nil
+  def to_s
+
+  end
+
+end
+
+class Node
+
+  def initialize
+
+  end
+
+  def value
+
+  end
+
+  def next_node
+
+  end
+
+end
